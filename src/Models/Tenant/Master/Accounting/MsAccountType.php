44ee2272f3313dfc0d8869ef7c7013e0c279504b
Wahyu Dwi Utomo <wahyuxd14@gmail.com>
2026-03-17 11:28:38 +0700

    fix model relation account type

--- src/Models/Tenant/Master/Accounting/MsAccountType.php
+++ src/Models/Tenant/Master/Accounting/MsAccountType.php
@@ -27,4 +27,14 @@ class MsAccountType extends BaseModelTenant
     {
         return $this->hasMany(MsCOA::class, 'account_type_id');
     }
+
+    public function coasMany()
+    {
+        return $this->belongsToMany(MsCOA::class)->using(MsAccountMappingType::class)->withPivot(['account_no', 'account_type', 'status', 'created_by', 'updated_by', 'json'])->withTimestamps();
+    }
+
+    // public function accountMappingTypes()
+    // {
+    //     return $this->hasMany(MsAccountMappingType::class, 'account_type_id');
+    // }
 }
