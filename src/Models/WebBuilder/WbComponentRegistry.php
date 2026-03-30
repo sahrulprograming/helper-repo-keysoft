f1ee9ea0d98ac88c21c40b560b7dba096080c009
sahrulprograming <sahrulsaefulah@gmail.com>
2026-03-30 08:21:40 +0700

    update model

--- src/Models/WebBuilder/WbComponentRegistry.php
+++ src/Models/WebBuilder/WbComponentRegistry.php
@@ -2,14 +2,13 @@
 
 namespace Keysoft\HelperLibrary\Models\WebBuilder;
 
-use App\Traits\AuditedBy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class WbComponentRegistry extends Model
 {
-    use SoftDeletes, AuditedBy;
+    use SoftDeletes;
     public const TABLE_NAME = 'wb_components_registry';
     public const RELATION_CATEGORY = 'registryCategory';
     protected $table = self::TABLE_NAME;
